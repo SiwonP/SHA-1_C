89291fcd50f07eb5180044fab03e2cb780e38f3c
Simon Petit <nomisp96@hotmail.fr>
2017-07-24 19:48:04 +0200

    initial commit"

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "message.h"
+
+int main(int argc, char *argv[]) {
+    unsigned int *message;
+
+    if (argc < 2) {
+        printf("usage : %s <string>", argv[0]);
+        return 1;
+    }
+    message = complete(argv[1]);
+    return 1;
+}
