7d6baa3a1ab8ffd3e924da374ed9b4db82a0d382
sopiU6 15 <nomisp96@hotmail.fr>
2017-07-25 16:13:52 +0200

    basic function

--- hash.c
+++ hash.c
@@ -2,3 +2,23 @@
 #include <stdio.h>
 #include "message.h"
 #include "hash.h"
+
+unsigned int ch(unsigned int x, unsigned int y, unsigned int z)
+{
+    return ((x & y) | (~x & z));
+}
+
+unsigned int parity(unsigned int x, unsigned int y, unsigned int z)
+{
+    return (x ^ y ^ z);
+}
+
+unsigned int maj(unsigned int x, unsigned int y, unsigned int z)
+{
+    return ((x & y) | (x & z) | (y & z));
+}
+
+unsigned int left_rotate(unsigned int x, unsigned int c)
+{
+    return ((x << c) | (x >> (32 - c)));
+}
--- hash.h
+++ hash.h
@@ -1,5 +1,11 @@
 #ifndef HASH_H
 #define HASH_H
+unsigned int ch(unsigned int, unsigned int, unsigned int);
 
+unsigned int parity(unsigned int, unsigned int, unsigned int);
+
+unsigned int maj(unsigned int, unsigned int, unsigned int);
+
+unsigned int left_rotate(unsigned int, unsigned int);
 
 #endif
